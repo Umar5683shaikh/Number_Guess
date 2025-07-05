1e4d253a98b39c71430b8672e27b58530204b449
Umar5683shaikh <umar5683shaikh@gmail.com>
2025-07-05 15:26:02 +0530

    Upload
    Guess Number

---
+++ Number Guess/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_Guess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random sairaj = new Random();
+            int num = sairaj.Next(1,100);
+
+            bool key = false;
+
+            Console.WriteLine("Welcome to the Number Guess Game...");
+            
+            do
+            {
+                Console.WriteLine("Please Guess the Number...");
+                string input = Console.ReadLine();
+
+                int inp = Convert.ToInt32(input);
+
+                if (inp > num) 
+                {
+                    Console.WriteLine("Guess lower....");
+                }
+                else if (inp < num)
+                {
+                    Console.WriteLine("Guess higher....");
+                }
+                else if (inp == num)
+                {
+                    Console.WriteLine("You Win...");
+                    key = true;
+                }
+            } 
+            while (key == false);
+            Console.WriteLine("Thank You");
+            Console.ReadKey();
+        }
+    }
+}
